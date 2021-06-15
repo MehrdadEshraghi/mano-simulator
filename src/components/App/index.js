b989e0d89e9f4abb6b31a6805831b9525cc331cc
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-06-16 03:39:33 +0430

    render RAMTable component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import CodeForm from '../CodeForm';
+import ComputerState from '../ComputerState';
+import RAMTable from '../RAMTable';
+import './styles.css';
 
 function App() {
 	return (
-		<div>
-			<h1>App component</h1>
+		<div className="App">
+			<CodeForm />
+			<ComputerState />
+			<RAMTable />
 		</div>
 	);
 }
