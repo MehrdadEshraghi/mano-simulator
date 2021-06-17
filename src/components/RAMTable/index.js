951165ce2791fe55b24bd6d678df68051e2d7605
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-06-18 01:35:40 +0430

    add scroll effect and update table values

--- src/components/RAMTable/index.js
+++ src/components/RAMTable/index.js
@@ -1,24 +1,74 @@
-import { render } from '@testing-library/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'reactstrap';
+import { Link, Element } from 'react-scroll';
 
-const RAMTable = ({ code, infoTable }) => {
+const RAMTable = ({ infoTable }) => {
+	console.log(infoTable);
+
+	useEffect(() => {
+		try {
+			document.querySelector('#link-scroll').click();
+		} catch (error) {}
+	});
+
+	const getCodeRow = (address) => {
+		for (let i = 0; i < infoTable.length; i++) if (infoTable[i].address === address) return infoTable[i];
+	};
+
 	const renderTable = () => {
-		if (code.length) {
+		if (infoTable.length) {
+			const rows = [];
+			for (let i = 0; i < 4096; i++) {
+				const codeRow = getCodeRow(i.toString(16).toUpperCase());
+				if (codeRow) rows.push(codeRow);
+				else rows.push({ label: null, address: i.toString(16).toUpperCase(), instruction: '', HEX: '0000' });
+			}
+			let flag = true;
+			console.log(rows);
 			return (
-				<div style={{ overflowY: 'scroll', width: '30%', alignSelf: 'flex-start', marginTop: '50px', height: '90vh' }}>
-					<Table dark>
-						<thead>
-							<tr>
-								<th>Label</th>
-								<th>Address</th>
-								<th>Instruction</th>
-								<th>Hex</th>
-							</tr>
-						</thead>
-						<tbody>
-							{infoTable.map((t, index) => {
-								if (t.instruction !== 'END' && t.instruction.split(' ')[0] !== 'ORG')
+				<React.Fragment>
+					<div onClick={() => console.log('clicked')}>
+						<Link
+							style={{ display: 'none' }}
+							id="link-scroll"
+							containerId="my-container"
+							to="my-scroll"
+							smooth={true}
+							duration={500}
+						/>
+					</div>
+					<Element
+						id="my-container"
+						style={{
+							overflowY: 'scroll',
+							width: '30%',
+							alignSelf: 'flex-start',
+							marginTop: '50px',
+							height: '90vh'
+						}}
+					>
+						<Table dark>
+							<thead>
+								<tr>
+									<th>Label</th>
+									<th>Address</th>
+									<th>Instruction</th>
+									<th>Hex</th>
+								</tr>
+							</thead>
+							<tbody>
+								{rows.map((t, index) => {
+									if (t.instruction && flag) {
+										flag = false;
+										return (
+											<tr name="my-scroll" key={index}>
+												<td>{t.label}</td>
+												<td>{t.address}</td>
+												<td>{t.instruction}</td>
+												<td>{t.HEX}</td>
+											</tr>
+										);
+									}
 									return (
 										<tr key={index}>
 											<td>{t.label}</td>
@@ -27,10 +77,11 @@ const RAMTable = ({ code, infoTable }) => {
 											<td>{t.HEX}</td>
 										</tr>
 									);
-							})}
-						</tbody>
-					</Table>
-				</div>
+								})}
+							</tbody>
+						</Table>
+					</Element>
+				</React.Fragment>
 			);
 		} else {
 			return null;
