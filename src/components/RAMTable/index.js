71728f6b76100359658b5144973558a4e5b9853b
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-06-17 05:28:12 +0430

    fill table by code info

--- src/components/RAMTable/index.js
+++ src/components/RAMTable/index.js
@@ -1,203 +1,42 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import { Table } from 'reactstrap';
 
-const RAMTable = (props) => {
-	return (
-		<div style={{ overflowY: 'scroll', width: '30%', alignSelf: 'flex-start', marginTop: '23px', height: '90vh' }}>
-			<Table dark>
-				<thead>
-					<tr>
-						<th>Label</th>
-						<th>Address</th>
-						<th>Instruction</th>
-						<th>Hex</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-						<td>0321</td>
-					</tr>
-					<tr>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-						<td>0321</td>
-					</tr>
-				</tbody>
-			</Table>
-		</div>
-	);
+const RAMTable = ({ code, infoTable }) => {
+	const renderTable = () => {
+		if (code.length) {
+			return (
+				<div style={{ overflowY: 'scroll', width: '30%', alignSelf: 'flex-start', marginTop: '50px', height: '90vh' }}>
+					<Table dark>
+						<thead>
+							<tr>
+								<th>Label</th>
+								<th>Address</th>
+								<th>Instruction</th>
+								<th>Hex</th>
+							</tr>
+						</thead>
+						<tbody>
+							{infoTable.map((t, index) => {
+								if (t.instruction !== 'END' && t.instruction.split(' ')[0] !== 'ORG')
+									return (
+										<tr key={index}>
+											<td>{t.label}</td>
+											<td>{t.address}</td>
+											<td>{t.instruction}</td>
+											<td>{t.HEX}</td>
+										</tr>
+									);
+							})}
+						</tbody>
+					</Table>
+				</div>
+			);
+		} else {
+			return null;
+		}
+	};
+	return <React.Fragment>{renderTable()}</React.Fragment>;
 };
 
 export default RAMTable;
